fdf593576e9c8d1226b42211bcfaa3864480bc7c
fahad39039 <42373870+fahad39039@users.noreply.github.com>
2020-03-24 17:46:02 +0530

    Update asgi.py

--- src/mechat/asgi.py
+++ src/mechat/asgi.py
@@ -12,5 +12,5 @@ import os
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mechat.settings')
-
+django.setup()
 application = get_default_application()
